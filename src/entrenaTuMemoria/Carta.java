b66da22845454671f09aab6aeb7802f4b652375b
andresfeliper3 <andresfeliper.2001@gmail.com>
2020-08-30 17:28:46 -0500

    creación de clase Carta

---
+++ src/entrenaTuMemoria/Carta.java
@@ -0,0 +1,26 @@
+package entrenaTuMemoria;
+
+public class Carta {
+	
+	private String nombre;
+	private int posicion;
+	private boolean visible;
+	
+	//Constructor
+	public Carta(String nombre) {
+		this.nombre = nombre;
+		posicion = 0; //posición inicial, antes de comenzar el juego
+		visible = true;
+	}
+	
+	//Hace que una carta deje de ser visible
+	public void voltearCarta() {
+		this.visible = false;
+	}
+	
+	//Retorna el nombre de la carta
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+}
